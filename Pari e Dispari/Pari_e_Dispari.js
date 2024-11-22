ec83745cbbd11f4580bb7295cc492d113c502354
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2024-11-22 19:52:11 +0100

    feat: add number random

--- Pari e Dispari/Pari_e_Dispari.js
+++ Pari e Dispari/Pari_e_Dispari.js
@@ -21,4 +21,11 @@ const sceltaUtente = prompt('Scegli: Pari o Dispari')
 console.log(numeriUtente);
 const numeriUtente = parseInt(prompt('Scegli un numero da 1 a 5'))
 
+// Creo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
 
+function numeroRandom() {
+  return Math.floor(Math.random() * 5) + 1;
+} 
+
+// Inserisco il numero random in una variabile
+const numeroCpu = numeroRandom();
